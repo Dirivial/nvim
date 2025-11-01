2eba069ba1c481905b6eabd41472c0f70ff663b1
Dirivial <alexander.kadeby@gmail.com>
2025-11-01 13:05:32 +0100

    Set tabstop to 4

--- init.lua
+++ init.lua
@@ -22,6 +22,8 @@ vim.o.number = true
 --  Experiment for yourself to see if you like it!
 vim.o.relativenumber = true
 
+vim.o.tabstop = 4
+
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
 
