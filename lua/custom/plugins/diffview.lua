1e7130057b3236c34d69385fec751130d0ae3abd
Dirivial <alexander.kadeby@gmail.com>
2026-06-23 15:38:34 +0200

    Stay on same line after opening a fold in diffview

--- lua/custom/plugins/diffview.lua
+++ lua/custom/plugins/diffview.lua
@@ -49,6 +49,45 @@ return {
       zi = 'Toggle folding',
     }
 
+    local function preserve_view(action, fold_cmd)
+      return function()
+        local win = vim.api.nvim_get_current_win()
+        local view = vim.fn.winsaveview()
+        local screen_row = vim.fn.winline()
+        local open_commands = {
+          za = true,
+          zA = true,
+          zo = true,
+          zO = true,
+          zr = true,
+          zR = true,
+        }
+        local fold_start = vim.fn.foldclosed '.'
+        local fold_end = vim.fn.foldclosedend '.'
+
+        if open_commands[fold_cmd] and fold_start > -1 and fold_end < vim.fn.line '$' then
+          view.lnum = fold_end + 1
+          screen_row = screen_row + 1
+        end
+
+        action()
+
+        if vim.api.nvim_win_is_valid(win) then
+          vim.api.nvim_win_call(win, function()
+            vim.fn.winrestview(view)
+
+            if screen_row > 1 then
+              vim.cmd.normal { 'zt', bang = true }
+
+              for _ = 2, screen_row do
+                vim.cmd.normal { '\x19', bang = true }
+              end
+            end
+          end)
+        end
+      end
+    end
+
     opts.keymaps = opts.keymaps or {}
     opts.keymaps.view = opts.keymaps.view or {}
 
@@ -56,7 +95,7 @@ return {
       table.insert(opts.keymaps.view, {
         mapping[1],
         mapping[2],
-        mapping[3],
+        preserve_view(mapping[3], mapping[2]),
         { desc = fold_desc[mapping[2]] or 'Diff fold command' },
       })
     end
